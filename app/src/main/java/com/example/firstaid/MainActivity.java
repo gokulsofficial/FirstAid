578b2f7a0fcd25e7995b1ede10fd774b60857ed9
Gokul Kannan <gokulkannan1602s@gmail.com>
2019-01-26 00:30:46 +0530

    First commit

--- app/src/main/java/com/example/firstaid/MainActivity.java
+++ app/src/main/java/com/example/firstaid/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.firstaid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv1, tv2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv1 = (TextView) findViewById(R.id.tips);
+        tv2 = (TextView) findViewById(R.id.hospital);
+
+        tv1.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(this, Tips.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+
 }
